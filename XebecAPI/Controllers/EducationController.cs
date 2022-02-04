7aa66487e0734961afc380601ccb01f059acc650
chux05 <45816186+chux05@users.noreply.github.com>
2022-02-04 14:10:42 +0200

    Update EducationController.cs
    update return method to reflect correct one

--- XebecAPI/Controllers/EducationController.cs
+++ XebecAPI/Controllers/EducationController.cs
@@ -32,7 +32,7 @@ namespace XebecAPI.Controllers
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> GetEducation()
+        public async Task<IActionResult> GetEducations()
         {
             try
             {
