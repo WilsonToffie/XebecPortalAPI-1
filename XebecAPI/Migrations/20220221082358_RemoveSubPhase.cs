cae3173f5c2fae49ff1b50a2fd6df0ced78788e2
Tshegofatso <Tshegofatso@LAP-JHB-005>
2022-02-21 12:40:50 +0200

    remove SubPhases & email template column

---
+++ XebecAPI/Migrations/20220221082358_RemoveSubPhase.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace XebecAPI.Migrations
+{
+    public partial class RemoveSubPhase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ApplicationSubPhases");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ApplicationSubPhases",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ApplicationPhaseId = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApplicationSubPhases", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ApplicationSubPhases_ApplicationPhases_ApplicationPhaseId",
+                        column: x => x.ApplicationPhaseId,
+                        principalTable: "ApplicationPhases",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ApplicationSubPhases_ApplicationPhaseId",
+                table: "ApplicationSubPhases",
+                column: "ApplicationPhaseId");
+        }
+    }
+}
--- XebecAPI/Shared/ApplicationPhase.cs
+++ XebecAPI/Shared/ApplicationPhase.cs
@@ -10,7 +10,6 @@ namespace XebecAPI.Shared
     {
         public int Id { get; set; }
         public string Description {  get; set; }
-        public string EmailTemplate { get; set; }
 
         public List<ApplicationPhaseHelper> PhaseHelpers { get; set; }
 
