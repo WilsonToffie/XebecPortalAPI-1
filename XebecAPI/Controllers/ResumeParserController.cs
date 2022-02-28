d97a8898b8c74a2e3561127f101c97bb916f6215
chux05 <45816186+chux05@users.noreply.github.com>
2022-02-28 18:26:28 +0200

    added try catches

--- XebecAPI/Controllers/ResumeParserController.cs
+++ XebecAPI/Controllers/ResumeParserController.cs
@@ -27,19 +27,19 @@ namespace XebecAPI.Controllers
         {
             StringBuilder output = new StringBuilder("Running python\n");
             Console.WriteLine("Setting python Evnironment\n");
-            
+
             //Setting up python environment
             await Installer.SetupPython();
             PythonEngine.Initialize();
-            
+
             //Installing modules
             Installer.TryInstallPip();
             Installer.PipInstallModule("spacy==2.3.7");
             Installer.PipInstallModule("PyMuPDF");
             //Installer.PipInstallModule("spacy-look-data");
-            
+
             dynamic spacy = PythonEngine.ImportModule("spacy");
-            
+
 
 
             output.AppendLine("Done !! Installing Spacy");
@@ -49,7 +49,7 @@ namespace XebecAPI.Controllers
 
             string filename = "Alice Clark CV.pdf";
 
-           
+
 
             string ResumeFilePath = System.IO.Path.Combine(Environment.CurrentDirectory, @"CVs");
 
@@ -64,15 +64,15 @@ namespace XebecAPI.Controllers
             dynamic fname =
                 $"{ResumeFilePath}/{filename}";
             StringBuilder text = new StringBuilder(ReadPDF(fname));
-            
+
             // dynamic doc2 = fitz.open(fname);
             // foreach (dynamic page in doc2)
             // {
             //     text.Append(page.get_text());
             // }
-            
-            
-            
+
+
+
             dynamic doc = nlp_model(text.ToString());
             string res = "{";
 
@@ -80,159 +80,177 @@ namespace XebecAPI.Controllers
             {
                 res += $" \"{ent.label_}\" : \"{ent.text.ToString()}\",";
             }
-     
+
             res = res.Substring(0, res.Length - 1);
             res += "}";
 
-             var test = JObject.Parse(res);
+            var test = JObject.Parse(res);
 
             return test;
 
         }
-        
+
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IActionResult))]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> UploadFile([FromForm] string url)
         {
-            if (!ModelState.IsValid)
-                return BadRequest("Invalid data.");
-            //Download file from Blob storage
-
-            AzureSasCredential credential = new AzureSasCredential(
-                "sp=racwdli&st=2022-02-28T08:30:27Z&se=2022-03-11T16:30:27Z&sv=2020-08-04&sr=c&sig=TE%2B2VCz%2B6KKFbYHIkQwxGPOYWVUtht3xBPYZ8bE3kH4%3D");
-            BlobClient blobClient = new BlobClient(new Uri(url), credential, new BlobClientOptions());
-
-            string downloadFilePath = Environment.CurrentDirectory + @"\Resumes";
-            if (Directory.Exists(downloadFilePath))
+            try
             {
-                Directory.CreateDirectory(downloadFilePath);
+                if (!ModelState.IsValid)
+                    return BadRequest("Invalid data.");
+                //Download file from Blob storage
+
+                AzureSasCredential credential = new AzureSasCredential(
+                    "sp=racwdli&st=2022-02-28T08:30:27Z&se=2022-03-11T16:30:27Z&sv=2020-08-04&sr=c&sig=TE%2B2VCz%2B6KKFbYHIkQwxGPOYWVUtht3xBPYZ8bE3kH4%3D");
+                BlobClient blobClient = new BlobClient(new Uri(url), credential, new BlobClientOptions());
+
+                string downloadFilePath = Environment.CurrentDirectory + @"\Resumes";
+                if (Directory.Exists(downloadFilePath))
+                {
+                    Directory.CreateDirectory(downloadFilePath);
+                }
+
+
+                //var res = await blobClient.DownloadToAsync(downloadFilePath);
+                Console.WriteLine($">>>>>>>>>>>>>>>>>>res path{Environment.CurrentDirectory} \n\ndownloadFilePath {downloadFilePath}<<<<<<<<<<<<<<<<<");
+                //return Ok("Resume bois");
+                Uri uri = new Uri(url);
+                string filename = System.IO.Path.GetFileName(uri.LocalPath);
+
+                string filePath = $"{downloadFilePath}/{filename}";
+
+                var result = blobClient.DownloadTo(filePath); // file is downloaded
+                                                              // check file download was success or not
+                if (result.Status == 206 || result.Status == 200)
+                {
+                    //string ress = TestSpacy(text);
+                    //return Ok($"{ress}");
+                    // You would be knowing this by now
+                    //return Ok(ReadPDF(filePath));
+                    StringBuilder output = new StringBuilder("Running python\n");
+                    Console.WriteLine();
+
+                    Console.WriteLine("Running python\n");
+                    //output.AppendLine( await SetupPython());
+                    //Console.WriteLine();
+                    //output.AppendLine(await InstallSpacy());
+
+
+                    Console.WriteLine("Setting python Evnironment\n");
+
+                    await Installer.SetupPython();
+                    PythonEngine.Initialize();
+                    dynamic sys = PythonEngine.ImportModule("sys");
+
+                    Console.WriteLine("Done !! Setting python Evnironment\n");
+                    output.AppendLine("Done !! Setting python Evnironment\n");
+                    output.AppendLine($"Python version:{sys.version}");
+
+                    await Installer.SetupPython();
+                    Installer.TryInstallPip();
+                    Installer.PipInstallModule("spacy==2.3.7");
+                    //Installer.PipInstallModule("spacy-look-data");
+                    PythonEngine.Initialize();
+                    dynamic spacy = PythonEngine.ImportModule("spacy");
+
+
+                    output.AppendLine("Done !! Installing Spacy");
+                    output.AppendLine($"Spacy version:{spacy.__version__}");
+
+                    dynamic nlp_model = spacy.load("nlp_model");
+                    Installer.PipInstallModule("PyMuPDF");
+                    dynamic fitz = PythonEngine.ImportModule("fitz");
+
+
+                    dynamic fname =
+                        $@"{downloadFilePath}\{filename}";
+                    dynamic doc2 = fitz.open(fname);
+
+                    StringBuilder text = new StringBuilder();
+
+                    foreach (dynamic page in doc2)
+                    {
+                        text.Append(page.get_text());
+                    }
+
+
+                    dynamic doc = nlp_model(text.ToString());
+                    string res = "{";
+
+                    foreach (dynamic ent in doc.ents)
+                    {
+                        res += $" \"{ent.label_}\" : \"{ent.text.ToString()}\",";
+
+                    }
+
+                    res = res.Substring(0, res.Length - 1);
+                    res += "}";
+                    Console.WriteLine(res);
+
+                    var test = JObject.Parse(res);
+
+                    return Ok(test);
+                }
+                return Ok($"{result.Status}");
             }
-           
-
-            //var res = await blobClient.DownloadToAsync(downloadFilePath);
-            Console.WriteLine($">>>>>>>>>>>>>>>>>>res path{Environment.CurrentDirectory} \n\ndownloadFilePath {downloadFilePath}<<<<<<<<<<<<<<<<<");
-            //return Ok("Resume bois");
-            Uri uri = new Uri(url);
-            string filename = System.IO.Path.GetFileName(uri.LocalPath);
-
-            string filePath = $"{downloadFilePath}/{filename}";
-
-            var result = blobClient.DownloadTo(filePath); // file is downloaded
-            // check file download was success or not
-            if (result.Status == 206 || result.Status == 200)
+            catch (Exception e)
             {
-                //string ress = TestSpacy(text);
-                //return Ok($"{ress}");
-                // You would be knowing this by now
-                //return Ok(ReadPDF(filePath));
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+            }
+        }
+
+        [HttpPost("upload2")]
+        public async Task<IActionResult> UploadFile2([FromForm] string url)
+        {
+            try
+            {
+
+
                 StringBuilder output = new StringBuilder("Running python\n");
                 Console.WriteLine();
-
                 Console.WriteLine("Running python\n");
                 //output.AppendLine( await SetupPython());
                 //Console.WriteLine();
                 //output.AppendLine(await InstallSpacy());
-
-
                 Console.WriteLine("Setting python Evnironment\n");
-
                 await Installer.SetupPython();
                 PythonEngine.Initialize();
                 dynamic sys = PythonEngine.ImportModule("sys");
-
                 Console.WriteLine("Done !! Setting python Evnironment\n");
                 output.AppendLine("Done !! Setting python Evnironment\n");
                 output.AppendLine($"Python version:{sys.version}");
-
                 await Installer.SetupPython();
                 Installer.TryInstallPip();
                 Installer.PipInstallModule("spacy==2.3.7");
                 //Installer.PipInstallModule("spacy-look-data");
                 PythonEngine.Initialize();
                 dynamic spacy = PythonEngine.ImportModule("spacy");
-
-
                 output.AppendLine("Done !! Installing Spacy");
                 output.AppendLine($"Spacy version:{spacy.__version__}");
-
                 dynamic nlp_model = spacy.load("nlp_model");
                 Installer.PipInstallModule("PyMuPDF");
-                dynamic fitz = PythonEngine.ImportModule("fitz");
-
-
-                dynamic fname =
-                    $@"{downloadFilePath}\{filename}";
-                dynamic doc2 = fitz.open(fname);
-
+                dynamic fname = url;
+                //dynamic doc2 = fitz.open(fname);
                 StringBuilder text = new StringBuilder();
-
-                foreach (dynamic page in doc2)
-                {
-                    text.Append(page.get_text());
-                }
-
-                
-                dynamic doc = nlp_model(text.ToString());
+                dynamic doc = nlp_model(ReadPDF(fname));
                 string res = "{";
-
                 foreach (dynamic ent in doc.ents)
                 {
                     res += $" \"{ent.label_}\" : \"{ent.text.ToString()}\",";
-                    
                 }
-     
                 res = res.Substring(0, res.Length - 1);
                 res += "}";
                 Console.WriteLine(res);
-
                 var test = JObject.Parse(res);
-
                 return Ok(test);
             }
-            return Ok($"{result.Status}");
-        }
-
-        [HttpPost("upload2")]
-        public async Task<IActionResult> UploadFile2([FromForm] string url)
-        {
-            StringBuilder output = new StringBuilder("Running python\n");
-            Console.WriteLine();
-            Console.WriteLine("Running python\n");
-            //output.AppendLine( await SetupPython());
-            //Console.WriteLine();
-            //output.AppendLine(await InstallSpacy());
-            Console.WriteLine("Setting python Evnironment\n");
-            await Installer.SetupPython();
-            PythonEngine.Initialize();
-            dynamic sys = PythonEngine.ImportModule("sys");
-            Console.WriteLine("Done !! Setting python Evnironment\n");
-            output.AppendLine("Done !! Setting python Evnironment\n");
-            output.AppendLine($"Python version:{sys.version}");
-            await Installer.SetupPython();
-            Installer.TryInstallPip();
-            Installer.PipInstallModule("spacy==2.3.7");
-            //Installer.PipInstallModule("spacy-look-data");
-            PythonEngine.Initialize();
-            dynamic spacy = PythonEngine.ImportModule("spacy");
-            output.AppendLine("Done !! Installing Spacy");
-            output.AppendLine($"Spacy version:{spacy.__version__}");
-            dynamic nlp_model = spacy.load("nlp_model");
-            Installer.PipInstallModule("PyMuPDF");
-            dynamic fname = url;
-            //dynamic doc2 = fitz.open(fname);
-            StringBuilder text = new StringBuilder();
-            dynamic doc = nlp_model(ReadPDF(fname));
-            string res = "{";
-            foreach (dynamic ent in doc.ents)
+            catch (Exception e)
             {
-                res += $" \"{ent.label_}\" : \"{ent.text.ToString()}\",";
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
             }
-            res = res.Substring(0, res.Length - 1);
-            res += "}";
-            Console.WriteLine(res);
-            var test = JObject.Parse(res);
-            return Ok(test);
         }
 
 
@@ -251,20 +269,40 @@ namespace XebecAPI.Controllers
 
             return text;
         }
-        
+
         [HttpGet("getallfiles")] //Please modify
-        public string GetAllFiles(){
-            string resumefolder = Environment.CurrentDirectory + @"\Resumes";
-            DirectoryInfo d = new DirectoryInfo(resumefolder); //Assuming Test is your Folder
-            FileInfo[] Files = d.GetFiles("*.pdf"); //Getting pdf files
-            string str = "";
-            foreach(FileInfo file in Files )
+        public string GetAllFiles()
+        {
+            try
             {
-                str = str + "\n " + file.Name;
+
+                string str = "";
+                string resumefolder = Environment.CurrentDirectory;
+                DirectoryInfo d = new DirectoryInfo(resumefolder); //Assuming Test is your Folder
+                DirectoryInfo[] child = d.GetDirectories();
+
+                foreach (DirectoryInfo file in child)
+                {
+                    str = str + "\n " + file.FullName;
+                }
+
+                str += str + "\n Files +\n ";
+
+                FileInfo[] Files = d.GetFiles(); //Getting pdf files
+            
+                foreach (FileInfo file in Files)
+                {
+                    str = str + "\n " + file.Name;
+                }
+                return str;
             }
-            return str;
+            catch (Exception e)
+            {
+
+                return e.Message;
+            }
         }
-        
+
         [HttpGet("getCurrent")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
@@ -278,9 +316,9 @@ namespace XebecAPI.Controllers
             {
                 return e.Message;
             }
-            
+
         }
-        
+
         [HttpGet("GetBaseDir")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
@@ -288,15 +326,15 @@ namespace XebecAPI.Controllers
         {
             try
             {
-                   return $"Physical location {AppDomain.CurrentDomain.BaseDirectory}";
+                return $"Physical location {AppDomain.CurrentDomain.BaseDirectory}";
             }
             catch (Exception e)
             {
                 return e.Message;
             }
-            
+
         }
-        
+
         [HttpGet("AppContext")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
@@ -304,16 +342,16 @@ namespace XebecAPI.Controllers
         {
             try
             {
-                   return $"AppContext.BaseDir {AppContext.BaseDirectory}";
+                return $"AppContext.BaseDir {AppContext.BaseDirectory}";
             }
             catch (Exception e)
             {
                 return e.Message;
             }
-            
+
         }
-        
-           
-           
+
+
+
     }
 }
