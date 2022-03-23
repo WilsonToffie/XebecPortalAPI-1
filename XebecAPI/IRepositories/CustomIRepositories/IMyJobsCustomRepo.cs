6148345dc6167eeff3212bb1f00baeed46a92124
DBailie <DanielB@nebula.co.za>
2022-03-23 12:00:53 +0200

    MyJobs endpoint

---
+++ XebecAPI/IRepositories/CustomIRepositories/IMyJobsCustomRepo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using XebecAPI.Shared;
+
+namespace XebecAPI.IRepositories
+{
+    public interface IMyJobsCustomRepo
+    {
+        Task<List<Application>> GetAllApplicationDetails(int AppUserId);
+    }
+}
