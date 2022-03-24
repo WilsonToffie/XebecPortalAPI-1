a3fbfcb9726763ce5dc1a84865466e4f4c1538b3
chux05 <45816186+chux05@users.noreply.github.com>
2022-03-24 11:41:11 +0200

    Update UserDb.cs
    double negative

--- XebecAPI/Repositories/UserDb.cs
+++ XebecAPI/Repositories/UserDb.cs
@@ -174,7 +174,7 @@ namespace XebecAPI.Repositories
 				var user = await unitOfWork.AppUsers.GetT(q => q.Email.Equals(email));// WATCH OUT
 				var result = mapper.Map<AppUserDTO>(user);
 
-				if (!result.Registered == false)
+				if (!result.Registered)
 					return new AppUser()
 					{
 						Id = -2
