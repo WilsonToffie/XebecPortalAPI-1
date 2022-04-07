e2aeb5ba81e6ca21d1d78eb4544d5e81838b91ab
chux05 <45816186+chux05@users.noreply.github.com>
2022-04-07 11:32:09 +0200

    Added Auth using directive

--- XebecAPI/Controllers/PersonalInformationController.cs
+++ XebecAPI/Controllers/PersonalInformationController.cs
@@ -9,7 +9,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using XebecAPI.DTOs;
-
+using Microsoft.AspNetCore.Authorization;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
