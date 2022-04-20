a34ff14d2da52d49bd165f893d4ea03c40d49f95
Tshegofatso <Tshegofatso@LAP-JHB-005>
2022-04-20 12:48:23 +0200

    Revert "Workflow name change and auth change"
    This reverts commit 2b988bddb41c30161196600762b7bf65c3b372f2.

--- XebecAPI/Controllers/Security/AuthController.cs
+++ XebecAPI/Controllers/Security/AuthController.cs
@@ -16,7 +16,6 @@ using Microsoft.AspNetCore.Authentication;
 using System.Net.Http;
 using Newtonsoft.Json;
 using System.Net.Http.Json;
-using Microsoft.Extensions.Configuration;
 
 namespace XebecAPI.Controllers
 {
@@ -26,19 +25,17 @@ namespace XebecAPI.Controllers
 		private readonly IUserDb userDb;
 		private readonly IUnitOfWork unitOfWork;
         private readonly IEmailRepo emailrepo;
-        private readonly IConfiguration config;
 
-        public AuthController(IUserDb userDb, IUnitOfWork unitOfWork, IEmailRepo emailrepo, IConfiguration _config)
+        public AuthController(IUserDb userDb, IUnitOfWork unitOfWork, IEmailRepo emailrepo)
 		{
 			this.userDb = userDb;
 			this.unitOfWork = unitOfWork;
             this.emailrepo = emailrepo;
-            config = _config;
         }
 
 		private string CreateJWT(AppUser user)
 		{
-			var secretkey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(config["JWT:Key"]));
+			var secretkey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("THIS IS THE SECRET KEY"));
 			// NOTE: SAME KEY AS USED IN Startup.cs FILE
 
 			var credentials = new SigningCredentials(secretkey, SecurityAlgorithms.HmacSha256);
@@ -54,7 +51,7 @@ namespace XebecAPI.Controllers
 				// NOTE: this could a unique ID assigned to the user by a database
 			};
 
-			var token = new JwtSecurityToken(issuer: config["JWT:Issuer"], audience: config["JWT:Audience"], claims: claims, expires: DateTime.Now.AddMinutes(15), signingCredentials: credentials);
+			var token = new JwtSecurityToken(issuer: "domain.com", audience: "domain.com", claims: claims, expires: DateTime.Now.AddMinutes(60), signingCredentials: credentials);
 			return new JwtSecurityTokenHandler().WriteToken(token);
 		}
 
